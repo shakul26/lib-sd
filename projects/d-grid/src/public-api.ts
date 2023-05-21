791901e3cf8fcee98f403d5895b7b3f8cfa7448d
Shakul Dubey <shakuldubey@gmail.com>
2023-05-21 11:16:01 +0530

    fix: removing unused poroject

--- projects/d-grid/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of d-grid
- */
-
-export * from './lib/d-grid.service';
-export * from './lib/d-grid.component';
-export * from './lib/d-grid.module';
--- projects/insta-grid/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of insta-grid
- */
-
-export * from './lib/insta-grid.service';
-export * from './lib/insta-grid.component';
-export * from './lib/insta-grid.module';
